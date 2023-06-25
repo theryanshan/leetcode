c8785b41bca6c1b6d5594024d39301e711c29f5f
Ryan Shan <ryanshan28@gmail.com>
2023-06-25 12:32:02 -0700

    704. Binary Search

---
+++ 704_binary_search.py
@@ -0,0 +1,17 @@
+# Time: O(log(n))
+# Space: O(1)
+# Note: use mid = left + (right - left) // 2 to avoid overflow
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
