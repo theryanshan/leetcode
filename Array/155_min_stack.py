4615044e2cd121ddb61b6c7bab01a2c94fcaca6c
Ryan Shan <ryanshan28@gmail.com>
2023-09-06 00:07:15 -0700

    155. Min Stack

---
+++ Array/155_min_stack.py
@@ -0,0 +1,32 @@
+# Tip: Consider keeping track of a min value for each element push in the stack, so that we can get the min value in O(1) time
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1]) if self.minStack else val
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        if len(self.stack):
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
