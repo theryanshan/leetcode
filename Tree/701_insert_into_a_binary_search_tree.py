931c977e6557948eee0b2ef643325eca9f6baa6e
Ryan Shan <ryanshan28@gmail.com>
2023-09-20 23:37:07 -0700

    701. Insert into a Binary Search Tree

---
+++ Tree/701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,22 @@
+# Time O(height of tree)
+# Space O(height of tree)
+# recursion rule: the method place the node in the right place, and return the root at each level
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
